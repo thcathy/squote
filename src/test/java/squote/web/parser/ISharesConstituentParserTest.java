00bb774b3f1d7367747b98c47fec3713afd678e1
thcathy <thcathy@gmail.com>
2015-03-03 21:42:04 +0800

    Update test as MSCI HK changes

--- src/test/java/squote/web/parser/ISharesConstituentParserTest.java
+++ src/test/java/squote/web/parser/ISharesConstituentParserTest.java
@@ -21,8 +21,8 @@ public class ISharesConstituentParserTest {
 	//@Test
 	public void testGetMSCIHKConstituent() {
 		List<String> result = ISharesConstituentParser.parseMSCIHK();
-		assertTrue("MSCI China index should contain 13",result.contains("13"));
-		assertTrue("MSCI China index should contain 1",result.contains("1"));
+		assertTrue("MSCI China index should contain 5",result.contains("5"));
+		assertTrue("MSCI China index should contain 2",result.contains("2"));
 		assertTrue("MSCI China index should contain 388",result.contains("388"));
 		assertTrue("MSCI China index should contain over 30 stocks", result.size() > 30);
 		
