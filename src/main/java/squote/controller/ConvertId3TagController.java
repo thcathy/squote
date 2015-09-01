6fe02ea28e342b971e757884cb3efc66b920db4b
thcathy <thcathy@gmail.com>
2015-09-01 21:36:53 +0800

    add mp3 file to test

--- src/main/java/squote/controller/ConvertId3TagController.java
+++ src/main/java/squote/controller/ConvertId3TagController.java
@@ -2,6 +2,7 @@ package squote.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -47,7 +48,7 @@ public class ConvertId3TagController {
 		List<Map<String, String>> tags = files.stream()
 			.map(f -> previewConvert(f, fromEncoding, toEncoding))
 			.collect(Collectors.toList());
-		modelMap.put("tags", tags);
+		modelMap.put("tagList", tags);
 		
 		return "id3tag";
 	}
@@ -55,42 +56,50 @@ public class ConvertId3TagController {
 	private Map<String, String> previewConvert(File f, String fromEncoding, String toEncoding) {
 		Map<String, String> tags = new HashMap<>();
 		tags.put("FilePath", f.getAbsolutePath());
-				
+		
 		try {
 			Mp3File mp3file = new Mp3File(f);
 			if (mp3file.hasId3v1Tag()) {
 	        	ID3v1 id3v1Tag = mp3file.getId3v1Tag();
-	        	tags.put("Track"	, new String(id3v1Tag.getTrack().getBytes(fromEncoding), toEncoding));
-	        	tags.put("Artist"	, new String(id3v1Tag.getArtist().getBytes(fromEncoding), toEncoding));
-	        	tags.put("Title"	, new String(id3v1Tag.getTitle().getBytes(fromEncoding), toEncoding));
-	        	tags.put("Album"	, new String(id3v1Tag.getAlbum().getBytes(fromEncoding), toEncoding));
-	        	tags.put("Year"		, new String(id3v1Tag.getYear().getBytes(fromEncoding), toEncoding));	        	
-	        	tags.put("GenreDesc", new String(id3v1Tag.getGenreDescription().getBytes(fromEncoding), toEncoding));
-	        	tags.put("Comment"	, new String(id3v1Tag.getComment().getBytes(fromEncoding), toEncoding));
+	        	tags.put("Track"	, convertEncoding(id3v1Tag.getTrack(),fromEncoding, toEncoding));
+	        	tags.put("Artist"	, convertEncoding(id3v1Tag.getArtist(),fromEncoding, toEncoding));
+	        	tags.put("Title"	, convertEncoding(id3v1Tag.getTitle(),fromEncoding, toEncoding));
+	        	tags.put("Album"	, convertEncoding(id3v1Tag.getAlbum(),fromEncoding, toEncoding));
+	        	tags.put("Year"		, convertEncoding(id3v1Tag.getYear(),fromEncoding, toEncoding));	        	
+	        	tags.put("GenreDesc", convertEncoding(id3v1Tag.getGenreDescription(),fromEncoding, toEncoding));
+	        	tags.put("Comment"	, convertEncoding(id3v1Tag.getComment(),fromEncoding, toEncoding));
 	        }
 			
 			if (mp3file.hasId3v2Tag()) {
 	        	ID3v2 id3v2Tag = mp3file.getId3v2Tag();
-	        	tags.put("Track2"			, new String(id3v2Tag.getTrack().getBytes(fromEncoding), toEncoding));
-	        	tags.put("Artist2"			, new String(id3v2Tag.getArtist().getBytes(fromEncoding), toEncoding));
-	        	tags.put("Title2"			, new String(id3v2Tag.getTitle().getBytes(fromEncoding), toEncoding));
-	        	tags.put("Album2"			, new String(id3v2Tag.getAlbum().getBytes(fromEncoding), toEncoding));
-	        	tags.put("Year2"			, new String(id3v2Tag.getYear().getBytes(fromEncoding), toEncoding));	        	
-	        	tags.put("GenreDesc2"		, new String(id3v2Tag.getGenreDescription().getBytes(fromEncoding), toEncoding));
-	        	tags.put("Composer"			, new String(id3v2Tag.getComposer().getBytes(fromEncoding), toEncoding));
-	        	tags.put("Publisher"		, new String(id3v2Tag.getPublisher().getBytes(fromEncoding), toEncoding));
-	        	tags.put("Original artist"	, new String(id3v2Tag.getOriginalArtist().getBytes(fromEncoding), toEncoding));
-	        	tags.put("Album artist"		, new String(id3v2Tag.getAlbumArtist().getBytes(fromEncoding), toEncoding));
-	        	tags.put("Copyright"		, new String(id3v2Tag.getCopyright().getBytes(fromEncoding), toEncoding));
-	        	tags.put("URL"				, new String(id3v2Tag.getUrl().getBytes(fromEncoding), toEncoding));
-	        	tags.put("Encoder"			, new String(id3v2Tag.getEncoder().getBytes(fromEncoding), toEncoding));	        	
+	        	tags.put("Track2"			, convertEncoding(id3v2Tag.getTrack(),fromEncoding, toEncoding));
+	        	tags.put("Artist2"			, convertEncoding(id3v2Tag.getArtist(),fromEncoding, toEncoding));
+	        	tags.put("Title2"			, convertEncoding(id3v2Tag.getTitle(),fromEncoding, toEncoding));
+	        	tags.put("Album2"			, convertEncoding(id3v2Tag.getAlbum(),fromEncoding, toEncoding));
+	        	tags.put("Year2"			, convertEncoding(id3v2Tag.getYear(),fromEncoding, toEncoding));	        	
+	        	tags.put("GenreDesc2"		, convertEncoding(id3v2Tag.getGenreDescription(),fromEncoding, toEncoding));
+	        	tags.put("Composer"			, convertEncoding(id3v2Tag.getComposer(),fromEncoding, toEncoding));
+	        	tags.put("Publisher"		, convertEncoding(id3v2Tag.getPublisher(),fromEncoding, toEncoding));
+	        	tags.put("Original artist"	, convertEncoding(id3v2Tag.getOriginalArtist(),fromEncoding, toEncoding));
+	        	tags.put("Album artist"		, convertEncoding(id3v2Tag.getAlbumArtist(),fromEncoding, toEncoding));
+	        	tags.put("Copyright"		, convertEncoding(id3v2Tag.getCopyright(),fromEncoding, toEncoding));
+	        	tags.put("URL"				, convertEncoding(id3v2Tag.getUrl(),fromEncoding, toEncoding));
+	        	tags.put("Encoder"			, convertEncoding(id3v2Tag.getEncoder(),fromEncoding, toEncoding));	        	
 	        }	        	        
 		} catch (Exception e) {
 			log.warn("Cannot process mp3: " + f.getAbsolutePath(), e);			
 			tags.put("Exception", e.getMessage());
 		}
-		
+		log.debug(tags.toString());
 		return tags;
+	}
+	
+	private String convertEncoding(String input, String from, String to) throws UnsupportedEncodingException {
+		if (StringUtils.isBlank(input)) return "";
+		return new String(input.getBytes(from), to);
+		
+		//ByteBuffer bb = Charset.forName(from).encode(input);
+        //return Charset.forName(to).decode(bb).toString();
 	}
 
 	private boolean noInput(String action, String folder) {		
